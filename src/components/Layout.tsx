a3ff2bed0269f7ca9b6019569986fdfb46453d45
Anton Ball <antonball@gmail.com>
2019-11-10 20:36:25 +0800

    Feature: Adding favicon and transition to anchors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useSiteMetadata } from '../Hooks/UseSiteMetadata'
 import './style.css'
-import { Link } from 'gatsby'
+import { Link, withPrefix } from 'gatsby'
 import { Logo } from './Logo/Logo'
 
 export const Layout: React.FC = ({ children }) => {
@@ -15,32 +15,32 @@ export const Layout: React.FC = ({ children }) => {
                 <title>{title}</title>
                 <meta name="description" content={description} />
 
-                {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href={`${withPrefix('/')}img/apple-touch-icon.png`}
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href={`${withPrefix('/')}img/favicon-32x32.png`}
+                    sizes="32x32"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href={`${withPrefix('/')}img/favicon-16x16.png`}
+                    sizes="16x16"
+                />
 
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
+                <link
+                    rel="mask-icon"
+                    href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+                    color="#ff4400"
+                />
+                <meta name="theme-color" content="#f47100" />
 
-        <meta property="og:type" content="business.business" />
+                {/* <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
         <meta
