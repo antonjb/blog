0f7dc8c1dd8652ab7ae680b51dd6eb9735e6ae29
Anton <antonball@gmail.com>
2025-07-26 10:16:21 +0800

    Box component
    Adds the box component and updates the custom properties to have an
    actual 0 value.

--- src/types/space.d.ts
+++ src/types/space.d.ts
@@ -1,7 +1,9 @@
-export type Space = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+export type Space = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+export type BorderSize = '0' |'1' | '2' | '3' | '4' | '5';
 
 declare module 'astro/types' {
     interface Props {
         space?: Space;
+        borderSize?: BorderSize;
     }
 } 
